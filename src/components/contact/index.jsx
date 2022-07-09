d2fe95ba1e6069b780bd7e73ece0187ce111ad86
Andrew Howran <andrewhowrandev17@gmail.com>
2022-07-09 10:33:39 -0400

    added validation to contact form

--- src/components/contact/index.jsx
+++ src/components/contact/index.jsx
@@ -1,46 +1,62 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
+import { validateEmail } from "../../utils/helpers.js";
 
 function Contact(props) {
-  // const [toSend, setToSend] = useState({
-  //   from_name: "",
-  //   to_name: "",
-  //   message: "",
-  //   reply_to: "",
-  // });
+  const [formState, setFormState] = useState({
+    user_name: "",
+    user_email: "",
+    message: "",
+  });
 
-  // const onSubmit = (e) => {
-  //   e.preventDefault();
-  //   {
-  //     /* --- METHOD TO SEND THE MAIL --- */
-  //   }
-  // };
+  const [errorMessage, setErrorMessage] = useState("");
 
-  // const handleChange = (e) => {
-  //   setToSend({ ...toSend, [e.target.name]: e.target.value });
-  // };
+  const { user_name, user_email, message } = formState;
+
+  const handleChange = (e) => {
+    if (e.target.name === "user_email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("Your email is invalid.");
+      } else {
+        setErrorMessage("");
+      }
+    } else {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required.`);
+      } else {
+        setErrorMessage("");
+      }
+    }
+    if (!errorMessage) {
+      setFormState({ ...formState, [e.target.name]: e.target.value });
+      console.log("Handle Form", formState);
+    }
+  };
 
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
-
-    emailjs
-      .sendForm(
-        "service_bbrsffm",
-        "template_8y9jxkw",
-        form.current,
-        "8268wx-YQfeyZJ_Gb"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          e.target.reset();
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    if (!errorMessage) {
+      console.log("email sent!");
+      // emailjs
+      //   .sendForm(
+      //     "service_bbrsffm",
+      //     "template_8y9jxkw",
+      //     form.current,
+      //     "8268wx-YQfeyZJ_Gb"
+      //   )
+      //   .then(
+      //     (result) => {
+      //       console.log(result.text);
+      //       e.target.reset();
+      //     },
+      //     (error) => {
+      //       console.log(error.text);
+      //     }
+      //   );
+    }
   };
 
   return (
@@ -64,7 +80,7 @@ function Contact(props) {
             id=""
             placeholder="Name"
             className="bg-slate-600 rounded-3xl p-3 pl-6 focus:outline-text_color outline-none"
-           
+            onBlur={handleChange}
           />
           <input
             type="email"
@@ -72,13 +88,7 @@ function Contact(props) {
             id=""
             placeholder="Email"
             className="bg-slate-600 rounded-3xl p-3 pl-6 focus:outline-text_color outline-none"
-          />
-          <input
-            type="text"
-            name="subject"
-            id=" "
-            placeholder="Subject"
-            className="bg-slate-600 rounded-3xl p-3 pl-6 focus:outline-text_color outline-none"
+            onBlur={handleChange}
           />
           <textarea
             name="message"
@@ -86,7 +96,11 @@ function Contact(props) {
             className="bg-slate-600 rounded-3xl p-3 pl-6 h-40 focus:outline-text_color outline-none"
             placeholder="Message"
             resize="none"
+            onBlur={handleChange}
           ></textarea>
+          <div>
+            {errorMessage && <div className="errorMsg">{errorMessage}</div>}
+          </div>
 
           <button className="self-end text-text_color border-text_color border-2 justify-center mr-10 rounded-lg px-3 py-1 bg-third hover:bg-text_color hover:text-third transition-all duration-200 ease-linear tracking-widest">
             SEND
